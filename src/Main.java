dad9b56da63eb3fcf1180614723bd7add1f35503
davouddkareshki <ostad1kareshki@gmail.com>
2026-02-09 18:47:07 +0330

    Updated main. import packages.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-import edu.ticket.TicketService;
+import edu.ticket.*;
+import edu.ticket.strategy.BusStrategy;
+import edu.ticket.strategy.FlightStrategy;
+import edu.ticket.state.PaidState;
 
 public class Main {
-
     public static void main(String[] args) {
+        System.out.println("--- Scenario 1: Bus Ticket (Successful) ---");
+        TicketService busTicket = new TicketService(new BusStrategy());
+        System.out.println("Price: " + busTicket.getPrice());
+        System.out.println("Desc: " + busTicket.getDescription());
 
-        TicketService ticketService = new TicketService();
+        busTicket.buy();
 
-        // Example 1: Bug reported from web
-        ticketService.handle("WEB", "BUG");
+        busTicket.buy();
 
-        System.out.println("------------");
+        System.out.println("\n--- Scenario 2: Flight Ticket (Cancelled) ---");
+        TicketService flightTicket = new TicketService(new FlightStrategy());
+        System.out.println("Price: " + flightTicket.getPrice());
 
-        // Example 2: Question reported via email
-        ticketService.handle("EMAIL", "QUESTION");
+        flightTicket.cancel();
+
+        flightTicket.buy();
     }
 }
